4731593babf9d2066a9ade6aeeb1f7224d751ddd
Sunny1135 <100459411+Sunny1135@users.noreply.github.com>
2022-03-19 16:43:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,75 @@
+var SpeechRecognition=window.webkitSpeechRecognition;
+var x= document.getElementById("myAudiomemo");
+var y=document.getElementById("myAudiolegends");
+var recognition=new SpeechRecognition();
+function start()
+{
+    document.getElementById("textbox").innerHTML="";
+    recognition.start();
+}
+recognition.onresult=function(event){
+console.log(event);
+var Content =event.results[0][0].transcript;
+console.log(Content);
+document.getElementById("textbox").innerHTML=Content;
+console.log(Content);
+if(Content=="take my selfie")
+{
+console.log("taking selfie---");
+speak();
+}
+if(Content=="play"){
+setTimeout(function()
+{
+    soundmemo();
+},3000);
+}
+if(Content=="chat")
+{
+    window.location="https://mahdihat791.github.io/v2/kwitter/index.html";
+}
+if(Content==Content&&MouseEvent)
+{
+var link="https://www.google.com/search?q="+Content;
+window.location=link;
+}
+}
+
+function speak()
+{
+var synth=window.speechSynthesis;
+speak_data="taking your selfie in 10 seconds";
+var utterThis=new SpeechSynthesisUtterance(speak_data);
+synth.speak(utterThis);
+Webcam.attach(camera);
+setTimeout(function()
+{
+take_snapshot();
+save()  ;
+},10000);
+}
+camera=document.getElementById("camera");
+Webcam.set({
+width:360,
+height:250,
+image_format:'jpeg',
+jpeg_quality:90
+});
+function take_snapshot()
+{
+Webcam.snap(function(data_uri)
+{
+document.getElementById("result").innerHTML="<img id='selfie_image' src='"+data_uri+"'>";
+});
+}
+function save()
+{
+link=document.getElementById("link");
+image=document.getElementById("selfie_image").src;
+link.href=image;
+link.click();
+}
+function soundmemo()
+{
+x.play();
+}
